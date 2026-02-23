2daa3eba981ee58a859abb4a7dc4693c88d5e306
ACortesPalomares <cortes.palomares.angello00@gmail.com>
2026-02-23 15:23:57 -0500

    Creacion plantilla scripts

---
+++ PrimerTallerCG/Assets/Game/Scripts/PaqueteDato.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+
+[System.Serializable]
+public class PaqueteDato
+{
+    
+    public string id;
+    public int tamanoCarga;
+    public float tiempoLlegada;
+
+
+    public PaqueteDato()
+    {
+    }
+    public PaqueteDato(string id, int tamanoCarga, float tiempoLlegada)
+    {
+        this.id = id;
+        this.tamanoCarga = tamanoCarga;
+        this.tiempoLlegada = tiempoLlegada;
+    }
+
+    public string Id { get => id; set => id = value; }
+    public int TamanoCarga { get => tamanoCarga; set => tamanoCarga = value; }
+    public float TiempoLlegada { get => tiempoLlegada; set => tiempoLlegada = value; }
+}
